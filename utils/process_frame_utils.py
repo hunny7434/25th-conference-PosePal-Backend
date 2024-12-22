00db776fad70829204528fefb84f4e417c069dab
Seoyoon Kim <syuna@yonsei.ac.kr>
2024-12-22 16:03:16 +0900

    update process frame utils

--- utils/process_frame_utils.py
+++ utils/process_frame_utils.py
@@ -1,4 +1,18 @@
+import cv2
+import mediapipe as mp
+
 # Placeholder function for processing a frame with a model
 def process_frame_with_model(frame):
     # Placeholder: Perform some processing on the frame
+    
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(frame_rgb)
+
+    if results.pose_landmarks:
+        #프레임에 랜드마크를 렌더링
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, 
+                                  mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=5, circle_radius=10), #점
+                                  mp_drawing.DrawingSpec(thickness=15, color=(0, 0, 255)))
+        
+
     return frame
